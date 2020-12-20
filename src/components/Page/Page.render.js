a28be8abe5c9e14a180bfc97a2f92704c7a2fb2c
halinaPP <galunikipp@gmail.com>
2020-12-20 23:06:43 +0300

    feat: set styles for main blocks

--- src/components/Page/Page.render.js
+++ src/components/Page/Page.render.js
@@ -1,8 +1,11 @@
+import { FAVICON_URL } from '@/constants/constants';
+import { renderFavicon } from '@/helpers/utils';
 import { renderHeader } from '../Header/Header.render';
 import { renderFooter } from '../Footer/Footer.render';
 import { renderMainContent } from '../Main/Main.render';
 
 export const renderPage = () => {
+    renderFavicon(FAVICON_URL);
     document.body.append(renderHeader());
     renderMainContent();
     document.body.append(renderFooter());

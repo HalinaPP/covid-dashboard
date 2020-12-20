7183941e7f440bde334b0838fa8e88b903ee255b
halinaPP <galunikipp@gmail.com>
2020-12-20 13:29:52 +0300

    fix: eslint rule

--- webpack.config.js
+++ webpack.config.js
@@ -12,161 +12,165 @@ const isProd = !isDev;
 const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
 
 const plugins = () => {
-  const base = [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-      minify: {
-        collapseWhitespace: isProd
-      }
-    }),
-    new CleanWebpackPlugin(),
+    const base = [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
 
-    new MiniCssExtractPlugin({
-      filename: filename('css')
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/assets/'),
-          to: path.resolve(__dirname, 'dist/assets')
-        }
-      ]
-    })
-  ];
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets/'),
+                    to: path.resolve(__dirname, 'dist/assets')
+                }
+            ]
+        })
+    ];
 
-  if (isProd) {
-    base.push(new BundleAnalyzerPlugin());
-  }
+    if (isProd) {
+        base.push(new BundleAnalyzerPlugin());
+    }
 
-  return base;
+    return base;
 };
 
 const optimization = () => {
-  const config = {
-    splitChunks: {
-      chunks: 'all'
-    }
-  };
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
 
-  if (isProd) {
-    config.minimizer = [new CssMinimizerPlugin(), new TerserPlugin()];
-  }
-  return config;
+    if (isProd) {
+        config.minimizer = [new CssMinimizerPlugin(), new TerserPlugin()];
+    }
+    return config;
 };
 
 const cssLoaders = (extra) => {
-  const loaders = [
-    {
-      loader: MiniCssExtractPlugin.loader,
-      options: {
-        publicPath: '/'
-      }
-    },
-    'css-loader'
-  ];
-  if (extra) {
-    loaders.push(extra);
-  }
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                publicPath: '/'
+            }
+        },
+        'css-loader'
+    ];
+    if (extra) {
+        loaders.push(extra);
+    }
 
-  return loaders;
+    return loaders;
 };
 
 const jsloader = () => {
-  const loaders = [
-    {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: ['@babel/plugin-proposal-class-properties']
-      }
-    }
-  ];
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-class-properties']
+            }
+        }
+    ];
 
-  if (isDev) {
-    loaders.push('eslint-loader');
-  }
-  return loaders;
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+    return loaders;
 };
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: {
-    main: ['@babel/polyfill', './index.js']
-  },
-  output: {
-    filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js', '.json', '.png'],
-    alias: {
-      '@models': path.resolve(__dirname, 'src/models'),
-      '@': path.resolve(__dirname, 'src')
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './index.js']
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+
+    optimization: optimization(),
+
+    devServer: {
+        port: 4200,
+        overlay: true,
+        open: true,
+        contentBase: path.resolve(__dirname, './dist')
+    },
+
+    devtool: isDev ? 'source-map' : false,
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+
+            {
+                test: /\.(?:ico|png|jpg|svg|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.(wav|mp3)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/audio/[hash][ext]'
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                use: jsloader()
+            }
+        ]
+    },
+    eslint: {
+        configFile: '.eslintrc',
+        fix: true
     }
-  },
-
-  optimization: optimization(),
-
-  devServer: {
-    port: 4200,
-    overlay: true,
-    open: true,
-    contentBase: path.resolve(__dirname, './dist')
-  },
-
-  devtool: isDev ? 'source-map' : false,
-  plugins: plugins(),
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: cssLoaders()
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: cssLoaders('sass-loader')
-      },
-
-      {
-        test: /\.(?:ico|png|jpg|svg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]'
-        }
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
-      },
-      {
-        test: /\.(wav|mp3)$/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'assets/audio/[hash][ext]'
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        use: jsloader()
-      }
-    ]
-  }
 };

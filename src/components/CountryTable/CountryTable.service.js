07ebf764d366ba824e911eb9bbc40ae025ea3361
halinaPP <galunikipp@gmail.com>
2020-12-24 01:58:27 +0300

    ref: chamge color in table

--- src/components/CountryTable/CountryTable.service.js
+++ src/components/CountryTable/CountryTable.service.js
@@ -1,9 +1,7 @@
 import { getMapinfo } from '@/services/Countries';
 import { store } from '@/redux/store';
-
-import { CASES, DEATHS, FULL_SCREEN, RECOVERY, WORLD_ID, WORLD_NAME } from '@/constants/constants';
-
-
+import { CASES, DEATHS, RECOVERY, WORLD_ID, WORLD_NAME } from '@/constants/constants';
+import { CASES_COLOR, DEATHS_COLOR, RECOVERY_COLOR } from '@/components/Map/map';
 import { renderFilter } from '@/components/Filter/Filter.render';
 import { createHtmlElement } from '@/helpers/utils';
 
@@ -18,28 +16,33 @@ export const setTableInfo = async () => {
     const table = document.body.querySelector('.country-data-table');
     const filter = document.body.querySelector('.filter');
     let wrapper = document.body.querySelector('.country-data-wrapper');
-    if(wrapper){
+    if (wrapper) {
         wrapper.remove();
     }
-    if(filter){
+    if (filter) {
         filter.remove();
     }
     const info = await getTableInfo();
     const { countryName, casesValueAmount } = info;
-    wrapper = createHtmlElement('div','country-data-wrapper')
-    wrapper.innerHTML = `<h3>${
-        countryName !== WORLD_ID ? countryName : WORLD_NAME
-    }</h3>
+    wrapper = createHtmlElement('div', 'country-data-wrapper');
+    wrapper.innerHTML = `<h3>${countryName !== WORLD_ID ? countryName : WORLD_NAME}</h3>
                         <div class="country-data-table-header">
-                            <div>${CASES}</div>
-                            <div>${DEATHS}</div>
-                            <div>${RECOVERY}</div>
-                        </div>
-                        <div class="country-data-table-data">
-                            <div>${casesValueAmount[CASES]}</div>
-                            <div>${casesValueAmount[DEATHS]}</div>
-                            <div>${casesValueAmount[RECOVERY]}</div>
+                            <div>
+                                ${CASES}:
+                                <span style="color:${CASES_COLOR}">${casesValueAmount[CASES]}</span>
+                            </div>
+                            <div>${DEATHS}: <span style="color:${DEATHS_COLOR}">${
+        casesValueAmount[DEATHS]
+    }</span></div>
+                            <div>${RECOVERY}: <span style="color:${RECOVERY_COLOR}">${
+        casesValueAmount[RECOVERY]
+    }</span></div>
                         </div>`;
+    /* <div class="country-data-table-data">
+                            <div>${casesValueAmount[CASES]}</div>
+                            <div></div>
+                            <div></div>
+                        </div>`; */
     table.appendChild(wrapper);
     table.prepend(renderFilter());
 };
